ffb5c0f577ae1a1e21c182782a6556f48a6a8749
zo-sia <83347373+zo-sia@users.noreply.github.com>
2021-06-11 16:57:15 +0200

    text
    fabuła + okienko końcowe

--- official-engine.py
+++ official-engine.py
@@ -1,11 +1,11 @@
 import pygame
 import time
 
+
 class Window:
     def __init__(self, width, height, name):
         self.sizeWin = width, height
         self.name = name
-
 
         self.nameWindow()
 
@@ -41,22 +41,21 @@ class Board(Square):
         self.j_size = j_size
         self.board = []
         self.nextMove = ""
-        self.list_obstacle=[
-                        [0, 3],
-                        [1, 0],
-                        [2, 0],
-                        [2, 5],
-                        [3, 2],
-                        [4, 2],
-                        [5, 4],
-                        [6, 0],
-                        ]
-        self.player_coordinates= [[self.a],[self.b]]
-        self.meta_coordinates = [6,1]
+        self.list_obstacle = [
+            [0, 3],
+            [1, 0],
+            [2, 0],
+            [2, 5],
+            [3, 2],
+            [4, 2],
+            [5, 4],
+            [6, 0],
+        ]
+        self.player_coordinates = [[self.a], [self.b]]
+        self.meta_coordinates = [6, 1]
         self.player_on_meta = False
         self.close_win = False
         self.finished_board = False
-
 
         for i in range(self.i_size):
             p = []
@@ -77,34 +76,35 @@ class Board(Square):
     def player(self):
         key = pygame.key.get_pressed()
         self.board[self.a][self.b].color = (00, 100, 100)
-        self.board[self.meta_coordinates[0]][self.meta_coordinates[-1]].color=(250,250,250)
+        self.board[self.meta_coordinates[0]
+                   ][self.meta_coordinates[-1]].color = (250, 250, 250)
 
         for p in range(len(self.list_obstacle)):
 
-            self.block_x=self.list_obstacle[p][0]
-            self.block_y=self.list_obstacle[p][-1]
+            self.block_x = self.list_obstacle[p][0]
+            self.block_y = self.list_obstacle[p][-1]
             self.board[self.block_x][self.block_y].color = (0, 0, 0)
-        if key[pygame.K_RIGHT] and self.a < self.i_size - 1 and self.nextMove != "V" and [self.a+1,self.b] not in self.list_obstacle:
+        if key[pygame.K_RIGHT] and self.a < self.i_size - 1 and self.nextMove != "V" and [self.a + 1, self.b] not in self.list_obstacle:
             self.a += 1
             self.nextMove = "V"
             self.moveNum += 1
-        if key[pygame.K_LEFT] and self.a > 0 and self.nextMove != "V" and [self.a-1,self.b] not in self.list_obstacle:
+        if key[pygame.K_LEFT] and self.a > 0 and self.nextMove != "V" and [self.a - 1, self.b] not in self.list_obstacle:
             self.a -= 1
             self.nextMove = "V"
             self.moveNum += 1
-        if key[pygame.K_DOWN] and self.b < self.j_size - 1 and self.nextMove != "H" and [self.a, self.b+1] not in self.list_obstacle:
+        if key[pygame.K_DOWN] and self.b < self.j_size - 1 and self.nextMove != "H" and [self.a, self.b + 1] not in self.list_obstacle:
             self.b += 1
             self.nextMove = "H"
             self.moveNum += 1
-        if key[pygame.K_UP] and self.b > 0 and self.nextMove != "H" and [self.a,self.b-1] not in self.list_obstacle:
+        if key[pygame.K_UP] and self.b > 0 and self.nextMove != "H" and [self.a, self.b - 1] not in self.list_obstacle:
             self.b -= 1
             self.nextMove = "H"
             self.moveNum += 1
 
-        self.player_coordinates = [self.a,self.b]
+        self.player_coordinates = [self.a, self.b]
         self.board[self.a][self.b].color = (200, 0, 100)
 
-        self.player_coordinates = [self.a,self.b]
+        self.player_coordinates = [self.a, self.b]
         if self.player_coordinates == self.meta_coordinates:
             self.player_on_meta = True
 
@@ -112,9 +112,9 @@ class Board(Square):
         return self.player_on_meta
 
     def end_player(self):
-        self.board[self.a][self.b].color = (0, 0, 100) #gracz meta
+        self.board[self.a][self.b].color = (0, 0, 100)  # gracz meta
         self.close_win = True
-        self.finished_board= True
+        self.finished_board = True
 
     def get_finished_board(self):
         return self.finished_board
@@ -122,11 +122,22 @@ class Board(Square):
     def get_close_win(self):
         return self.close_win
 
+def show_text():
+    print("\nZa siedmioma górami, za siedmioma lasami.\nW krainie wypełnionej z baśni postaciami:\nKsiążęta, królewny, smoki i wróżki,\nzłe stwory i wiedźma w ciele staruszki.\n")
+    print("Czas płynie przyjemnie w tym miejscu czarownym\ndopóty dopóki zło równa się z dobrym.\nLecz niebo się chmurzy, nadchodzi zagłada.\nWirus nadciąga z obcego nam świata.\n")
+    print("Posłuchaj wędrowcze, w potrzebie dopomóż\nŚpiesz się, gdyż wkrótce nie bedzie już komu.\nZ innego wymiaru zaraza doskwiera\nJedyna nadzieja w miksturze Pfizera!\n")
+    print("By znaleźć lekarstwo, pokonaj labirynt\nCiemności unikaj, tam mroczne są siły.\nStrzeż się uroku, bo z nikąd ratunku\ngdy pójdziesz dwa razy w tym samym kierunku!\n")
+    print("Jeśli jesteś gotów do drogi, naciśnij \"x\", a następnie kliknij w planszę.")
+
+
+
 def main():
-    pygame.init()
+    show_text()
+    ready=input()
+    if ready=="x":
+        pygame.init()
     window = Window(1000, 1000, "Plansza")
     window = pygame.display.set_mode(window.get_sizeWin())
-
 
     start_a = 0
     start_b = 5
@@ -143,10 +154,10 @@ def main():
             if event.type == pygame.QUIT:
                 run = False
                 end = time.time()
-                game_time = end-start
-                print(round(game_time,2))
-                print(board.moveNum)
-
+                game_time = end - start
+                print("Twoja podróż trwała ", round(game_time,2), "sekundy")
+                print("Wykonałeś: ", board.moveNum, "siedmiomilowych kroków")
+               
 
         window.fill((0, 0, 0))
         for i in range(x_size):
@@ -155,17 +166,18 @@ def main():
                     (board.get_sm()) * i + board.get_margin(),
                     (board.get_sm()) * j + board.get_margin(),
                     board.get_sizeSq()
-                    )
+                )
 
         board.player()
-        if board.get_player_on_meta()==True:
+        if board.get_player_on_meta() == True:
             board.end_player()
-            if board.get_finished_board()==True:
-                window.fill((250,250,250))
 
+            if board.get_finished_board() == True:
+                window.fill((250, 250, 250))
+                font1 = pygame.font.SysFont('Courier-New.ttf', 60)
+                text1 = font1.render('The end - zajrzyj do konsoli', True, (0, 0, 0))
+                window.blit(text1, (200,400))
 
         pygame.display.update()
 
-
 main()
-
